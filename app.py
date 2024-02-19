c3aa3c8e1dc6f952466a0926bde11ed8af68905e
Sanketh I <sanketh@yugabyte.com>
2024-02-19 11:47:55 -0800

    Stats maintenance

--- app.py
+++ app.py
@@ -3,53 +3,77 @@ import sys
 from flask import Flask, render_template, request, redirect, flash
 from requests import get
 from flask_sqlalchemy import SQLAlchemy
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'  # for database
 app.config['SECRET_KEY'] = 'So-Seckrekt'  # for flash messages
 db = SQLAlchemy(app)
-load_dotenv()
+#load_dotenv()
 
 
 class City(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=True, nullable=False)
 
+class Stats(db.Model):
+    route = db.Column(db.String(100), primary_key=True)
+    method = db.Column(db.String(100), primary_key=True)
+    day = db.Column(db.Date, primary_key=True)
+    count = db.Column(db.Integer, nullable=False)
+    
+class StatsManager():
+    def __init__(self):
+        pass
+
+    def inc(self, route, method):
+        app.app_context().push()
+        print(f"inc {route} {method}")
+        from datetime import date
+        day = date.today()
+        stats = db.session.query(Stats).filter(Stats.route == route, Stats.day == day).first()
+        if not stats:
+            new_row = Stats(route=route, method=method, day=day, count=1)
+            db.session.add(new_row)
+        else:
+            print(stats)
+
+            db.session.query(Stats).filter(Stats.route == route, Stats.day == day).update({Stats.count: Stats.count + 1})
+        db.session.commit()
+
+    def get_stats(self):
+        return db.session.query(Stats).all()
+
+statsmgr = StatsManager()
 
 @app.route('/')
 def index():
     cities = City.query.all()
-    weather_info_list = []
-    api_key = os.getenv('API_KEY')
-    for city in cities:
-        response = get(f'http://api.openweathermap.org/data/2.5/weather?q={city.name}&appid={api_key}')
-        if response.status_code == 200:
-            weather_info = response.json()
+    stats = statsmgr.get_stats()
+    for stat in stats:
+        print(f"Stats are {stat.method} {stat.route} {stat.count}")
+    return render_template('index.html', cities=cities)
 
-            dict_with_weather_info = {
-                'id': city.id,
-                'city': city.name,
-                'temp': round(weather_info['main']['temp'] - 273.15),
-                'state': weather_info['weather'][0]['main']
-            }
-            weather_info_list.append(dict_with_weather_info)
+@app.before_request
+def before_request_func():
+    print(f"before_request {request.endpoint}")
+    if request.endpoint == 'static':
+        return
 
-    return render_template('index.html', weather=weather_info_list)
-
+    from threading import Thread
+    t = Thread(target=statsmgr.inc, args=(request.path, request.method)) 
+    t.start()
 
 @app.route('/add', methods=['POST'])
 def add_city():
+    print(f"{request.form}")
     if request.method == 'POST':
         city = request.form['city']
 
         existing_city = City.query.filter_by(name=city).first()
         if existing_city:
             flash('The city has already been added to the list!')
-            return redirect('/')
-        elif not get(f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid=3e92ae34eb4da94c70cc1a85fecc58a6').status_code == 200:
-            flash("The city doesn't exist!")
             return redirect('/')
 
         new_city = City(name=city)
@@ -74,10 +98,10 @@ def delete_city(city_id):
 
 if __name__ == '__main__':
     with app.app_context():
-        db.create_all()
+      db.create_all()
 
-    if len(sys.argv) > 1:
-        arg_host, arg_port = sys.argv[1].split(':')
-        app.run(host=arg_host, port=arg_port)
-    else:
-        app.run()
+      if len(sys.argv) > 1:
+          arg_host, arg_port = sys.argv[1].split(':')
+          app.run(host=arg_host, port=arg_port)
+      else:
+          app.run()
